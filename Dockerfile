359054f91ffd5a5cac0b21a1c36053ba22e8ab58
yunlei <yunlei@mathildetech.com>
2015-09-25 14:28:20 +0800

    print application json

--- Dockerfile
+++ Dockerfile
@@ -3,11 +3,11 @@ FROM ubuntu
 MAINTAINER Dalei <guoyunlei#live.com>
 
 RUN apt-get update -y && apt-get install git python python-pip -y
-RUN cd /tmp \
-        && git clone https://github.com/daleione/saturn.git \
-        && cd saturn \
-        && pip install -r requirements.txt
+RUN mkdir /saturn
+COPY . /saturn
+WORKDIR /saturn
+RUN pip install -r requirements.txt
 
 EXPOSE 8888
 
-CMD ["python", "/tmp/saturn/hello.py"]
+CMD ["python", "hello.py"]
--- hello.py
+++ hello.py
@@ -2,6 +2,7 @@ import tornado.httpserver
 import tornado.ioloop
 import tornado.options
 import tornado.web
+import json
 
 from tornado.options import define, options
 
@@ -10,13 +11,18 @@ define("port", default=8888, help="run on the given port", type=int)
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, world")
+        self.write("hello world")
 
+class PringHttpHeader(tornado.web.RequestHandler):
+    def get(self):
+        self.write(self.request.method)
+        self.write(self.request.headers)
 
 def main():
     tornado.options.parse_command_line()
     application = tornado.web.Application([
         (r"/", MainHandler),
+        (r"/http-header/", PringHttpHeader),
     ])
     http_server = tornado.httpserver.HTTPServer(application)
     http_server.listen(options.port)
