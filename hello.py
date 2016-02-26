645f2a2d0a26f32b2bbe93c001d80e374af54909
yunlei <yunlei@mathildetech.io>
2016-02-26 16:22:18 +0800

    add log test api

--- hello.py
+++ hello.py
@@ -2,7 +2,8 @@ import tornado.httpserver
 import tornado.ioloop
 import tornado.options
 import tornado.web
-import json
+
+import time
 
 from tornado.options import define, options
 
@@ -13,16 +14,32 @@ class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("hello world")
 
+
 class PringHttpHeader(tornado.web.RequestHandler):
     def get(self):
         self.write(self.request.method)
         self.write(self.request.headers)
+
+
+class Logtest(tornado.web.RequestHandler):
+    def get(self):
+        filename = self.get_argument("file")
+        i = 0
+        while i < 100:
+            f = open(filename, 'a')
+            f.write(str(i))
+            i += 1
+            print i
+            f.close()
+            time.sleep(3)
+
 
 def main():
     tornado.options.parse_command_line()
     application = tornado.web.Application([
         (r"/", MainHandler),
         (r"/http-header/", PringHttpHeader),
+        (r"/log-test", Logtest),
     ])
     http_server = tornado.httpserver.HTTPServer(application)
     http_server.listen(options.port)
